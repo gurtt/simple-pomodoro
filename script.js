5add105e07610b3c1b85f7afd9d7c49cb39ca370
riley <6712553+rileysommerville@users.noreply.github.com>
2021-12-02 02:54:10 +1000

    add timer

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var seconds = 0;
 var interval;
+let timer = document.getElementById("timer");
+let cycles = document.getElementById("cycles");
+
+function nextInterval() {
+    countdown(25);
+}
 
 function countdown(mins) {
    seconds = mins*60 || 0;     
    interval = setInterval(function() {
         seconds--;
+        timer.innerText = `${Math.floor(seconds / 60)}:${seconds % 60}`;
         if(!seconds){
-             clearInterval(interval); 
-
+             clearInterval(interval);
         }
    },1000)
 }
